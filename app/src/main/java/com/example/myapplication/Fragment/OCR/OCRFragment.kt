0dc39a6623f0afff8d8b691a5f735e4c7029bba2
Jitsuhara <pikoru323@yahoo.co.jp>
2020-06-16 18:30:23 +0900

    OCRを使った文字読み取りに挑戦

--- app/src/main/java/com/example/myapplication/Fragment/OCR/OCRFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragment/OCR/OCRFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.myapplication.Fragment.BaseFragment
 import com.example.myapplication.R
 import com.googlecode.tesseract.android.TessBaseAPI
@@ -16,16 +17,19 @@ class OCRFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.initial_setting_fragment, container, false)
-
-        val bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test)
+        val view = inflater.inflate(R.layout.ocr_fragment, container, false)
+        val bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.a)
         val baseApi = TessBaseAPI()
         // initで言語データを読み込む
-        baseApi.init(context!!.getFilesDir().toString(), "jpn")
+        baseApi.init(activity?.getFilesDir().toString(), "jpn")
         // ギャラリーから読み込んだ画像をFile or Bitmap or byte[] or Pix形式に変換して渡してあげる
         baseApi.setImage(bitmap)
         // これだけで読み取ったテキストを取得できる
         val recognizedText = baseApi.utF8Text
+
+
+        val imgTextView = view.findViewById<TextView>(R.id.img_text)
+        imgTextView.text = recognizedText
         baseApi.end()
         return view
     }
